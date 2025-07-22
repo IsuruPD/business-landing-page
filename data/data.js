3601087e6b6fb6eabcbaf0e383d6e326065e39bc
Isuru Dissanayake <isuru098@gmail.com>
2025-07-22 14:13:52 +0530

    Refactor: Dynamic Product Item Display v1
    Data structure modification,
    Dynamic product display page layout v1,
    Project folder structure modification

---
+++ data/data.js
@@ -0,0 +1,136 @@
+const projectItems = [
+    {
+      clientId: 1,
+      pageTitleMain: "Marina One",
+      pageTitleSub: "Singapore",
+      clientName: "Marina One, Singapore",
+      clientImage: "m1.jpg",
+      clientDesc: "Marina One, Singapore Marina One, Singapore Marina One, Singapore Marina One, Singapore Marina One, Singapore.",
+      productItems: [
+        {   
+            prodId: 1,
+            prodName: "Public Dashboard",
+            prodNameAbbr: "PD",
+            prodImgPaths: [
+                {
+                    prodImgId: 1.1,
+                    imgName:"",
+                    imgDesc:"",
+                    path:"MarinaOne/PD/pd1.jpg", 
+                },
+                {
+                    prodImgId: 1.2,
+                    imgName:"",
+                    imgDesc:"",
+                    path:"MarinaOne/PD/pd2.png", 
+                },
+            ],
+        },
+        {   
+            prodId: 2,
+            prodName: "Aircon Extension System",
+            prodNameAbbr: "AES",
+            prodImgPaths: [
+                {
+                    prodImgId: 2.1,
+                    imgName:"Dashboard",
+                    imgDesc:"",
+                    path:"MarinaOne/AES/dasboard.png", 
+                },
+                {
+                    prodImgId: 2.2,
+                    imgName:"Extension Page",
+                    imgDesc:"",
+                    path:"MarinaOne/AES/extension page.png", 
+                },
+                {
+                    prodImgId: 2.3,
+                    imgName:"Extension Report",
+                    imgDesc:"",
+                    path:"MarinaOne/AES/extension report.png", 
+                },
+                {
+                    prodImgId: 2.4,
+                    imgName:"New Extension",
+                    imgDesc:"",
+                    path:"MarinaOne/AES/new extension.png", 
+                },
+                {
+                    prodImgId: 2.5,
+                    imgName:"New Standing Order",
+                    imgDesc:"",
+                    path:"MarinaOne/AES/new standing order.png", 
+                },
+                {
+                    prodImgId: 2.6,
+                    imgName:"Standing Order Page",
+                    imgDesc:"",
+                    path:"MarinaOne/AES/standing order page.png", 
+                },
+                {
+                    prodImgId: 2.7,
+                    imgName:"Standing Order Report",
+                    imgDesc:"",
+                    path:"MarinaOne/AES/standing order report.png", 
+                },
+                {
+                    prodImgId: 2.8,
+                    imgName:"View Extension",
+                    imgDesc:"",
+                    path:"MarinaOne/AES/view extension.png", 
+                },
+                {
+                    prodImgId: 2.9,
+                    imgName:"View Standing Order",
+                    imgDesc:"",
+                    path:"MarinaOne/AES/view standing order.png", 
+                },
+            ],
+        },
+        {   
+            prodId: 3,
+            prodName: "Tenant Billing System",
+            prodNameAbbr: "TBS",
+            prodImgPaths: [
+                {
+                    prodImgId: 3.1,
+                    imgName:"Dashboard",
+                    imgDesc:"",
+                    path:"MarinaOne/TBS/TBS1.png", 
+                },
+                {
+                    prodImgId: 3.2,
+                    imgName:"",
+                    imgDesc:"",
+                    path:"MarinaOne/TBS/TBS2.png", 
+                },
+                {
+                    prodImgId: 3.3,
+                    imgName:"",
+                    imgDesc:"",
+                    path:"MarinaOne/TBS/TBS3.png", 
+                },
+                {
+                    prodImgId: 3.4,
+                    imgName:"",
+                    imgDesc:"",
+                    path:"MarinaOne/TBS/Config1.png", 
+                },
+                {
+                    prodImgId: 3.5,
+                    imgName:"",
+                    imgDesc:"",
+                    path:"MarinaOne/TBS/Config2.png", 
+                },
+                {
+                    prodImgId: 3.6,
+                    imgName:"",
+                    imgDesc:"",
+                    path:"MarinaOne/TBS/Config3.png", 
+                },
+            ],
+        },
+      ],
+    }
+  ];
+  
